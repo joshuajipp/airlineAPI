46cbde23f679d196ba69cc9e878a81bea2c77744
joshuajipp <joshuajipp@gmail.com>
2023-11-26 21:04:49 -0700

    fixing cors

---
+++ src/main/java/com/ENSF480/airlineBackend/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.ENSF480.airlineBackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
+
